713a2f20b58900436f6a84856469b1f062632e42
PlattenEcho <akunsekolahdika@gmail.com>
2024-08-07 21:43:49 +0700

    add kkn information on profike page

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -35,50 +35,76 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.all(10),
-      color: kWhiteColor,
-      child: _user == null
-          ? Center(child: CircularProgressIndicator())
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(10),
+        color: kWhiteColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _user == null
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      gapH24,
+                      const Icon(
+                        Icons.account_circle,
+                        size: 150,
+                      ),
+                      Text(
+                        _user!.namaLengkap,
+                        style: blackTextStyle.copyWith(
+                            fontSize: 20, fontWeight: bold),
+                      ),
+                      Text(
+                        _user!.jabatan,
+                        style: blackTextStyle.copyWith(fontSize: 16),
+                      ),
+                      gapH12,
+                      Text(
+                        'Username: ${_user!.username}',
+                        style: blackTextStyle.copyWith(fontSize: 16),
+                      ),
+                      gapH24,
+                      Button(
+                          text: "Log Out",
+                          textColor: kWhiteColor,
+                          startColor: kRedColor,
+                          endColor: kRedColor,
+                          onPressed: () {
+                            StorageController storageController =
+                                Get.find<StorageController>();
+                            storageController.removeData('isLoggedIn');
+                            storageController.removeData('user');
+                            Navigator.pushReplacementNamed(
+                                context, '/start-screen');
+                            showToast(context, "Anda telah berhasil logout");
+                          })
+                    ],
+                  ),
+            Column(
               children: [
-                gapH24,
-                const Icon(
-                  Icons.account_circle,
-                  size: 150,
+                Text(
+                  'Dibuat oleh KKN TIM II UNDIP 2023/2024',
+                  textAlign: TextAlign.center,
+                  style: blackTextStyle.copyWith(fontSize: 14),
                 ),
                 Text(
-                  _user!.namaLengkap,
-                  style:
-                      blackTextStyle.copyWith(fontSize: 20, fontWeight: bold),
+                  'DESA GUNUNGBATU',
+                  textAlign: TextAlign.center,
+                  style: blackTextStyle.copyWith(fontSize: 14),
                 ),
                 Text(
-                  _user!.jabatan,
-                  style: blackTextStyle.copyWith(fontSize: 16),
+                  'IG @journey.gunungbatu',
+                  style: blackTextStyle.copyWith(fontSize: 14),
                 ),
-                gapH12,
-                Text(
-                  'Username: ${_user!.username}',
-                  style: blackTextStyle.copyWith(fontSize: 16),
-                ),
-                gapH24,
-                Button(
-                    text: "Log Out",
-                    textColor: kWhiteColor,
-                    startColor: kRedColor,
-                    endColor: kRedColor,
-                    onPressed: () {
-                      StorageController storageController =
-                          Get.find<StorageController>();
-                      storageController.removeData('isLoggedIn');
-                      storageController.removeData('user');
-                      Navigator.pushReplacementNamed(context, '/start-screen');
-                      showToast(context, "Anda telah berhasil logout");
-                    })
               ],
             ),
+          ],
+        ),
+      ),
     );
   }
 }
